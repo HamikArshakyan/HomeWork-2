fe73db69ba20db48cccb8448eedc72d61ed8a258
HamikArshakyan <100372194+HamikArshakyan@users.noreply.github.com>
2022-03-16 01:40:27 +0400

    Update Task.js

--- Task.js
+++ Task.js
@@ -3,17 +3,11 @@
 let a = +prompt()
 let b = +prompt()
 let c = +prompt()
- if (a+b>c || a+c>b || b+c>a){ console.log ("Yes")
+ if (a+b>c && a+c>b && b+c>a){ console.log ("Yes")
                              }
 else{console.log("No")}
 
 
-// Task 2/2
-let a = +prompt()
-let b = +prompt()
-let c = +prompt()
- if (a+b>c){ console.log ("Yes")
-                             }
-else{console.log("No")}
+
 
 Davit jan mnacac tarberakner@ porceci bayc mots chstacvec
