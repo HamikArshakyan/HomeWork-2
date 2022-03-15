da1fb60f0f331186a80f869f4c78b805ca0dff6e
HamikArshakyan <100372194+HamikArshakyan@users.noreply.github.com>
2022-03-16 01:38:46 +0400

    Add files via upload

---
+++ Task.js
@@ -0,0 +1,19 @@
+//Task 2/1
+
+let a = +prompt()
+let b = +prompt()
+let c = +prompt()
+ if (a+b>c || a+c>b || b+c>a){ console.log ("Yes")
+                             }
+else{console.log("No")}
+
+
+// Task 2/2
+let a = +prompt()
+let b = +prompt()
+let c = +prompt()
+ if (a+b>c){ console.log ("Yes")
+                             }
+else{console.log("No")}
+
+Davit jan mnacac tarberakner@ porceci bayc mots chstacvec
